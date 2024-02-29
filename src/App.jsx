60bafdaea615ec8e5e8956a65cec0894230a198c
JosephYoon13 <jseokkiyoon@gmail.com>
2024-02-29 15:39:13 -0800

    added sidebar for page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Navbar from './components/navbar/Navbar';
 const App = () => {
   return (
     <div>
-      <section>
+      <section id='Home'>
         <Navbar />
       </section>
-      <section>Parallax Services</section>
+      <section id='Services'>Parallax</section>
+      <section>Services</section>
+      <section id='Portfolio'>Parallax</section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
-      <section>Contact</section>
+      <section id='Contact'>Contact</section>
     </div>
   );
 };
