0b0258c11a55ba8cccfd6b938168af55afa7745b
JosephYoon13 <jseokkiyoon@gmail.com>
2024-02-29 13:57:13 -0800

    enveloped navbar into folder

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import './navbar.scss';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       {/* Sidebar */}
       <div className='wrapper'>
-        <span>Seok Ki Yoon</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1.0 }}
+          transtion={{ duration: 0.5 }}
+        >
+          Seok Ki Yoon
+        </motion.span>
         <div className='social'>
           <a href=''>
             <img src='/facebook.png' alt='' />
