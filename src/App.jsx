a75878220a301bf4cd00850ecde6e2675b81d067
JosephYoon13 <jseokkiyoon@gmail.com>
2024-03-02 13:46:01 -0800

    added cursor animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './app.scss';
 import Contact from './components/contact/Contact';
+import Cursor from './components/cursor/Cursor';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
@@ -8,6 +9,7 @@ import Projects from './components/projects/Projects';
 const App = () => {
   return (
     <div>
+      <Cursor />
       <section id='Home'>
         <Navbar />
         <Hero />
