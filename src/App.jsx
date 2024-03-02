d4f9f63224a80347b20167f682aacdb2d5b06526
JosephYoon13 <jseokkiyoon@gmail.com>
2024-03-02 13:32:05 -0800

    finished contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './app.scss';
+import Contact from './components/contact/Contact';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
@@ -19,7 +20,9 @@ const App = () => {
         <Parallax type='projects' />
       </section>
       <Projects />
-      <section id='Contact'>Contact</section>
+      <section id='Contact'>
+        <Contact />
+      </section>
     </div>
   );
 };
